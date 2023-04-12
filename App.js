96a73dc50328ba0bc774df3ec409f7bb7d7a5c8c
krunalSolanki1 <krunalsolucky121@gmail.com>
2023-04-12 22:10:57 +0530

    config

--- App.js
+++ App.js
@@ -8,35 +8,38 @@ require('./Config/database')
 require('dotenv').config();
 const mongoDbSession = require('connect-mongodb-session')(session);
 const store = new mongoDbSession({
-    uri : process.env.MONGO_URI,
-    collection : "sessions",
-    sameSite: 'none', 
+    uri: process.env.MONGO_URI,
+    collection: "sessions",
+    sameSite: 'none',
+    secure: true,
 });
 
 const cors = require('cors');
 const Product = require('./Schema/Product');
 
+const corsOptions = {
+  origin: ['https://melodic-mandazi-cf07e8.netlify.app'],
+  credentials: true,
+};
 
-app.use(cors({credentials: true, origin: '*'}));
-
-// app.use(cors({credentials: true, origin: 'https://euphonious-yeot-a494ba.netlify.app'}));
-
+app.use(cors(corsOptions));
   
 app.use(session({
-    secret : "hello muine",
-    resave : false,
-    saveUninitialized : false,
-    store : store,
-}))
+    secret: "hello muine",
+    resave: false,
+    saveUninitialized: false,
+    store: store,
+    cookie: {
+      sameSite: 'none',
+      secure: true,
+    },
+}));
 
-
-
-app.use(express.json())
+app.use(express.json());
 app.use('/product', ProductRouter);
-app.use('/user', UserRouter)
-app.use('/cart', CartRouter)
-
+app.use('/user', UserRouter);
+app.use('/cart', CartRouter);
 
 app.listen(process.env.PORT, () => {
     console.log(`Server is running on ${process.env.PORT}`);
-})
+});
